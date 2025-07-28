45cc9bfd3b4a65bd93d6c427f5455bcc6971be74
WoodShack <woodhousie@gmail.com>
2025-07-28 19:09:01 -0400

    Add object click detection

---
+++ js/game.js
@@ -0,0 +1,45 @@
+const gameImage = document.getElementById("game-img");
+const objectTable = document.getElementById("object-table-body");
+let objects = [];
+
+
+function checkClick(clickX,clickY){
+    for (let i = 0; i < objects.length; i++) {
+        if (clickX >= objects[i].topLeft[0] && clickX <= objects[i].bottomRight[0] && 
+            clickY >= objects[i].topLeft[1] && clickY <= objects[i].bottomRight[1]) {
+            objects[i].clicked = true;
+            console.log("Clicked "+objects[i].name);
+        }
+    }
+    
+    updateObjectTable();
+}
+
+function addObject(topLeft,bottomRight,name){
+    objects.push({
+        topLeft:topLeft,
+        bottomRight:bottomRight,
+        name:name,
+        clicked:false
+    });
+}
+
+function updateObjectTable(){
+    let html = "";
+    for(object of objects){
+        html += "<tr><td>"+object.name+"</td><td>"+object.clicked+"</td></tr>";
+    }
+    objectTable.innerHTML = html;
+}
+
+addObject([168,81],[315,134],"Tiger");
+addObject([13,17],[88,75],"Star");
+addObject([125,374],[173,437],"Mushroom");
+updateObjectTable();
+
+gameImage.addEventListener('click', function(event) {
+    const rect = this.getBoundingClientRect(); // position of the image
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    checkClick(x,y);
+});
